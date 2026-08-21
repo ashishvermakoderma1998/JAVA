a9634b445c3c5a275c19e7e39452b4ab52a5975e
Ashish Kumar Verma <ashishvermakoderma@gmail.com>
2026-08-21 05:34:27 +0530

    first java code

---
+++ firstlevel.java
@@ -0,0 +1,169 @@
+public class firstlevel {
+    public static void main(String[] args) {
+        // --------1-----------
+        boolean isLightOn = true;
+        System.out.println(isLightOn);
+
+        // ----------2------------
+        int marks = 65;
+        boolean haspassed = marks >= 35;
+        System.out.println(haspassed);
+
+        // -----------3----------------
+        boolean isWeakend = true;
+        System.out.println("Time to Relax" + isWeakend);
+
+        // -------------4-----------------
+        boolean isOffline = true;
+        System.out.println(!isOffline);
+
+        // -----------5-----------
+        boolean hasTicket = true;
+        boolean hasId = true;
+        System.out.println(hasTicket && hasId);
+
+        // -----------6----------------
+        String name = "Ashish Kumar Verma";
+        int age = 28;
+        String city = "Koderma";
+        System.out.println("my name is " + name + " I am " + age + " years old and I live in " + city);
+
+        // -------------7----------------
+        String FavoriteMovie = "KGF";
+        String FavoriteFood = "Pizza";
+        System.out.println("My Favorite Movies is " + FavoriteMovie + " and my Favorite Food is " + FavoriteFood);
+
+        // -----------8----------------
+        char myNameInitial = 'A';
+        String myName = "Ashish";
+        System.out.println(myNameInitial + " " + myName);
+
+        // -------------9----------------
+        String name1 = "Ashish";
+        String domain = "example.com";
+        String email = name1 + "@" + domain;
+        System.out.println(email);
+
+        // -------------10----------------
+        char Symbol = '$';
+        System.out.println(Symbol);
+
+        // --------06august--------------
+        // ---------1--------------
+
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);
+
+        // ---------2------------
+        float num1 = 30.5f;
+        float num2 = 20.4f;
+        float result = num1 - num2;
+        System.out.println(result);
+
+        // ----------3------------
+        int numb = 14;
+        System.out.println(numb % 2 == 0 ? "Even" : "Odd");
+
+        // -----------4-----------------
+        int bonus = 30;
+        bonus += 10;
+        System.out.println("bonus" + " " + bonus);
+
+        // ----------5--------------
+        System.out.println(7 / 2);
+        System.out.println(7.0 / 2);
+
+        // ----------6----------------
+        int y = 5;
+        y++;
+        System.out.println(y);
+
+        // ------------7--------------
+        int L = 45;
+        int M = 35;
+        System.out.println(L > M ? "greater" : "smaller");
+
+        // --------------8----------------
+        int z = 7 / 4 * 9 / 2;
+        System.out.println(z);
+
+      
+
+        // --------Section 12----------
+        // ---------1------------
+    
+        int j = 10;
+        int k = 3;
+        System.out.println(j / k);
+
+        // -----------2-------------
+        System.out.println(10%3);
+
+        // ----------3-----------
+        int p = 7;
+        int q = ++p * 8;
+        System.out.println(q);
+
+        // -----------4-------------
+        char ab = 'B';
+             ab++;
+        System.out.println(ab);
+
+        // ---------5---------------
+
+        System.out.println(5 == 5.0);
+
+        // ------------6--------------
+
+        int m = 10;
+            m += 5;
+            m *= 2;
+        System.out.println(m);
+
+            // --------------7--------------
+            boolean results = (5>3) && (2>1); 
+            System.out.println(results);
+
+            // -----------8---------------
+            String s = "5" + 3 + 2;
+            System.out.println(s);
+
+            // ----------9-------------
+            int aa = 5;
+            System.out.println(aa++ + ++aa);
+
+            // ---------10----------------
+            int ae = 7;
+            int af = 7 * 49 / 7 + 35 / 7;
+            System.out.println(af);
+
+            // ----------11-------------
+            int citizenAge = 20;
+            boolean isCitizen = true;
+            boolean canVote = (citizenAge >= 18) && isCitizen;
+            System.out.println(canVote);
+
+            // ----------12--------------
+            boolean isMember = false;
+            boolean CoupanApplied = true;
+            boolean getDiscount = isMember || CoupanApplied;
+            System.out.println(getDiscount);
+
+            // ------------13----------------
+              int zz = 5;
+            System.out.println(zz++ + ++zz);
+
+            // ------------14----------------
+            
+
+
+
+
+
+
+
+
+
+    }
+}
