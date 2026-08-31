8071a6a5ae754fc5c464351d100675cb65d19c5f
Ashish Kumar Verma <ashishvermakoderma@gmail.com>
2026-08-31 15:55:39 +0530

    update

--- math.java
+++ math.java
@@ -5,7 +5,7 @@ public static void main(String[] args){
 
 Scanner sc = new Scanner(System.in);
  
-String userName = "Ashish Kumar Verma";
+String userName = "Ashish Verma";
 String city = "Koderma";
 System.out.println("Hello " + userName + " from "+ city);
 
