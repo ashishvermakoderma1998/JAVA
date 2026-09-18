a4dc9e9129aca0a38f51eb0d6d360de38d304272
Ashish <ashishvermakoderma@gmail.com>
2026-09-18 07:33:13 +0530

    update code

--- scanner.java
+++ scanner.java
@@ -190,7 +190,7 @@ public class scanner {
             //  Take two numbers from the user and print the larger of the two.
 
 
-            System.out.println("E First Number");
+            System.out.println("En First Number");
             int firstNumber1 = sc.nextInt();
             System.out.println("Enter Second Number");
             int secondNumber1 = sc.nextInt();
