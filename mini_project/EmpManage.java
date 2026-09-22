f1c64a286a23516e3897569f92074b2cd8b1c43a
Ashish <ashishvermakoderma@gmail.com>
2026-09-22 07:56:00 +0530

    update code

---
+++ mini_project/EmpManage.java
@@ -0,0 +1,68 @@
+package mini_project;
+import java.util.Scanner;
+
+class Employe{
+    String name;
+    double salary;
+
+    void work(){
+        System.out.println(name + " is working");
+    }
+}
+
+class Developer extends Employe {
+    void work(){
+        System.out.println(name + "is developing software");
+    }
+}
+
+class Tester extends Employe {
+    void work(){
+        System.out.println(name + "is testing software");
+    }
+}
+
+class Manager extends Employe {
+    void work(){
+        System.out.println(name + "is managing team");
+    }
+}
+
+
+
+
+public class EmpManage {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Developer developer = new Developer();
+
+        System.out.println("Enter Developer Name:");
+        developer.name = sc.nextLine();
+        System.out.println("Enter Developer Salary:");
+        developer.salary = sc.nextDouble();
+        sc.nextLine();
+
+        Tester tester = new Tester();
+        System.out.println("Enter Tester Name:");
+        tester.name = sc.nextLine();
+        System.out.println("Enter Tester Salary:");
+        tester.salary = sc.nextDouble();
+
+        Manager manager = new Manager();
+        System.out.println("Enter Manager Name:");
+        manager.name = sc.nextLine();
+        manager.name = sc.nextLine();
+        
+        System.out.println("Enter Manager:");
+        manager.salary = sc.nextDouble();
+
+        developer.work();
+        tester.work();
+        manager.work();
+
+
+
+        sc.close();
+    }
+ 
+}
