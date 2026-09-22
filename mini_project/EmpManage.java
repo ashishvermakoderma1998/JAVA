af3943bfeb1aadd697b12bf39dfdd612790c1cd8
Ashish <ashishvermakoderma@gmail.com>
2026-09-22 07:58:10 +0530

    update code

--- mini_project/EmpManage.java
+++ mini_project/EmpManage.java
@@ -53,7 +53,7 @@ public class EmpManage {
         manager.name = sc.nextLine();
         manager.name = sc.nextLine();
         
-        System.out.println("Enter Manager Sal:");
+        System.out.println("Enter Manager Sala:");
         manager.salary = sc.nextDouble();
 
         developer.work();
