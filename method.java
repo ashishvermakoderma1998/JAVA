7a2af69392e1e659ddb89b130791f88ce0648129
Ashish <ashishvermakoderma@gmail.com>
2026-09-25 10:10:40 +0530

    update

--- method.java
+++ method.java
@@ -2,7 +2,7 @@ public class method{
 
     // -------Create a method to print “Hello Java”--------
     public static void printMessage(){
-        System.out.println("Hello W");
+        System.out.println("Hello Wor");
     }
 
 // ---------Create a method to print your name.----------
