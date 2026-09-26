cf25c9336a90307f4635f85948a8abdcc40d7d35
Ashish <ashishvermakoderma@gmail.com>
2026-09-26 06:33:05 +0530

    update code

--- method.java
+++ method.java
@@ -7,7 +7,7 @@ public class method{
 
 // ---------Create a method to print your name.----------
 public static void printName(){
-    System.out.println("My name is Vikas");
+    System.out.println("My name is Vikash");
 }
 // ----------------Create a method that accepts a name and prints a greeting.----------
 public static void greet(String name){
